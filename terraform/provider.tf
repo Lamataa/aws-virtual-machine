11203b1bd6a9a11d390e4d9283413e0cb1762d2d
unknown <gb@hotmail.com>
2025-08-27 15:21:36 -0300

    Configuração inicial do Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,19 @@
-# PROVIDER
 terraform {
 
-  required_version = "~> 1.12.2"
+  required_version = "~> 1.13.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.9.0"
+      version = "~> 6.8.0"
     }
   }
 
-}
+  backend "s3" {
+    bucket       = "aws-s3-tfstate-lamata"
+    key          = "tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
 }
