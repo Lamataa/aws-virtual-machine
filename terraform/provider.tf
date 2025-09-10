80fffa84eb880d8e73a5ef81b2a29869e6aaa9ae
Gabriel-Lamata <rm562093@fiap.com.br>
2025-09-10 19:04:01 -0300

    teste7

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,23 @@
-# PROVIDER
+# terraform/provider.tf - Configuração do Provider AWS
+
 terraform {
-
   required_version = "1.13.1"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.11.0"
     }
   }
-
+  
   backend "s3" {
     bucket       = "aws-s3-tfstate-lamata"
     key          = "tfstate"
     region       = "us-east-1"
     use_lockfile = true
   }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
