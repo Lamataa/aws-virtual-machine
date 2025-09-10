d6c4be70882303b733a11cc6005c49d336d73044
Gabriel-Lamata <rm562093@fiap.com.br>
2025-09-10 19:06:27 -0300

    teste8

--- terraform/modules/vm/outputs.tf
+++ terraform/modules/vm/outputs.tf
@@ -14,3 +14,8 @@ output "instance_private_ip" {
   description = "IP privado da instância EC2"
   value       = aws_instance.instance.private_ip
 }
+
+output "instance_public_dns" {
+  description = "DNS público da instância EC2"
+  value       = aws_instance.instance.public_dns
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,23 @@
-output "ec2_public_url" {
-  value = "http://${aws_instance.instance.public_dns}"
-  description = "URL público da instância EC2"
+# outputs.tf - Outputs do módulo root
+
+# Outputs do módulo network
+output "vpc_id" {
+  description = "ID da VPC"
+  value       = module.network.vpc_id
+}
+
+output "subnet_id" {
+  description = "ID da subnet pública"
+  value       = module.network.subnet_id
+}
+
+# Outputs do módulo vm
+output "instance_id" {
+  description = "ID da instância EC2"
+  value       = module.vm.instance_id
+}
+
+output "instance_public_ip" {
+  description = "IP público da instância EC2"
+  value       = module.vm.instance_public_ip
 }
